73476cbdbcd99b4c79927873facce41dccb4d623
Khoi Dang <dangkhoihy1995@gmail.com>
2019-10-11 01:13:50 +0900

    no message

--- src/main/java/com/mkyong/security/SecurityService.java
+++ src/main/java/com/mkyong/security/SecurityService.java
@@ -2,23 +2,25 @@ package com.mkyong.security;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
 
-
-public class UserDetailSecurity{
-    
+@Service
+public class SecurityService{
+	@Autowired
     private AuthenticationManager authenticationManager;
 
-    
+	@Autowired
     private UserDetailsService userDetailsService;
 
-    private static final Logger logger = LoggerFactory.getLogger(UserDetailSecurity.class);
+    private static final Logger logger = LoggerFactory.getLogger(SecurityService.class);
 
-    
+    @Autowired
     public String findLoggedInUsername() {
         Object userDetails = SecurityContextHolder.getContext().getAuthentication().getDetails();
         if (userDetails instanceof UserDetails) {
@@ -28,7 +30,7 @@ public class UserDetailSecurity{
         return null;
     }
 
-    
+    @Autowired
     public void autoLogin(String username, String password) {
         UserDetails userDetails = userDetailsService.loadUserByUsername(username);
         UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails, password, userDetails.getAuthorities());
